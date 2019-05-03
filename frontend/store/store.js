98fdc38b9ccd2cca8491f0d95de698d0017a6ee6
aseemar <ajseemar@gmail.com>
2019-05-02 19:13:08 -0700

    Update store.js to remove logger from middleware when switching to development mode

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,8 +3,16 @@ import logger from 'redux-logger';
 import RootReducer from '../reducers/root';
 import thunk from '../middleware/thunk';
 
-const configureStore = (preloadedState = {}) => (
-    createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger))
-);
+const configureStore = (preloadedState = {}) => {
+    let middleware = [thunk];
+    if (process.env.NODE_ENV !== 'production') {
+        middleware = [...middleware, logger];
+    }
+    return createStore(
+        RootReducer,
+        preloadedState,
+        applyMiddleware(...middleware)
+        );
+    };
 
 export default configureStore;
